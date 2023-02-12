01d779ac826599bb272d22d328a5359a6bc78fd4
My Name <ccrpted@gmail.com>
2023-02-12 17:37:03 +0300

    Stage 2/5: Let’s get metric Description The examples you've printed are great, but they are not really helpful. Let's take a step further and create a program that converts kilometers to meters. Remember that one kilometer equals exactly 1000 meters.
    The program should prompt the user to enter the number of kilometers with the following message: "Enter a number and a measure: ". The user should input a number and a unit of measurement, for example, 2 km or 2 kilometers. Note that the units of measurement should be case insensitive, so "km", "KM", and "kM" should work just as fine as "km".
    
    The program should then output the equivalent of x kilometers in meters in the following format: x kilometers is y meters.
    
    If the name of the unit of measurement is entered incorrectly, the program should display: Wrong input.
    
    Objectives
    The program should convert the given number of kilometers to meters.
    The user can input "km", "kilometer" or "kilometers".
    The program should ask the user for input only once, not infinitely.
    If the value is 1, the name of the unit must be singular, otherwise, plural: "1 kilometer" but "2 kilometers".
    It is guaranteed that the number of kilometers is an integer.
    If the name of the unit of measurement is entered incorrectly, the program should display: Wrong input.

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -2,23 +2,112 @@ package converter
 
 fun main() {
 
-    println("Enter a number and a measure:")
+    println("Enter a number and a measure of length:")
     val list = readln().split(" ")
-    var num = list[0].toInt()
-    var measure = list[1]
+    var num = list[0].toFloat()
+    var measure = list[1].lowercase()
 
-    if (measure.lowercase() != "km" && measure.lowercase() != "kilometers" && measure.lowercase() != "kilometer") {
-        println("Wrong input")
-    } else {
-        reformat(num)
-    }
-}
+    val meters = mutableListOf("m", "meter", "meters")
+    val kilometers = mutableListOf("km", "kilometer", "kilometers")
+    val centimeters = mutableListOf("cm", "centimeter", "centimeters")
+    val millimeters = mutableListOf("mm", "millimeter", "millimeters")
+    val miles = mutableListOf("mi", "mile", "miles")
+    val yards = mutableListOf("yd", "yard", "yards")
+    val feet = mutableListOf("ft", "foot", "feet")
+    val inches = mutableListOf("in", "inch", "inches")
 
-fun reformat(km: Int) {
-    var result = km * 1000
-    if (km == 1) {
-        println("$km kilometer is $result meters")
-    } else {
-        println("$km kilometers is $result meters")
+    val metricList = mutableListOf(meters, kilometers, centimeters, millimeters, miles, yards, feet, inches)
+    fun calculate(num: Float, measure: String) {
+        var result = 0.0
+        var a = ""
+        var b = ""
+
+        when (measure) {
+            in meters -> result = num * 1.0
+            in kilometers -> result = num * 1000.0
+            in centimeters -> result = num * 0.01
+            in millimeters -> result = num * 0.001
+            in miles -> result = num * 1609.35
+            in yards -> result = num * 0.9144
+            in feet -> result = num * 0.3048
+            in inches -> result = num * 0.0254
+        }
+
+        if (measure in meters) {
+            if (num.toInt() == 1) {
+                a = meters[1]
+            } else {
+                a = meters[2]
+            }
+
+        }
+        if (measure in kilometers) {
+            if (num.toInt() == 1) {
+                a = kilometers[1]
+            } else {
+                a = kilometers[2]
+            }
+        }
+        if (measure in centimeters) {
+            if (num.toInt() == 1) {
+                a = centimeters[1]
+            } else {
+                a = centimeters[2]
+            }
+        }
+        if (measure in millimeters) {
+            if (num.toInt() == 1) {
+                a = millimeters[1]
+            } else {
+                a = millimeters[2]
+            }
+        }
+        if (measure in miles) {
+            if (num.toInt() == 1) {
+                a = miles[1]
+            } else {
+                a = miles[2]
+            }
+        }
+        if (measure in yards) {
+            if (num.toInt() == 1) {
+                a = yards[1]
+            } else {
+                a = yards[2]
+            }
+        }
+        if (measure in feet) {
+            if (num.toInt() == 1) {
+                a = feet[1]
+            } else {
+                a = feet[2]
+            }
+        }
+        if (measure in inches) {
+            if (num.toInt() == 1) {
+                a = inches[1]
+            } else {
+                a = inches[2]
+            }
+        }
+
+
+        if (result.toInt() == 1) {
+            b = "meter"
+        } else {
+            b = "meters"
+        }
+        println("$num $a is $result $b")
+
     }
-}
+
+
+
+        if (measure !in metricList.joinToString()) {
+            println("Wrong input. Unknown unit $measure")
+        } else {
+            calculate(num, measure)
+        }
+    }
+
+
